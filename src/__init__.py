77b3bcf881904c380d529f482aed6e31c3701f91
TylerWerne40 <TylerWerne40@gmail.com>
2025-07-19 18:31:50 -0600

    Add projects to portfolio as well as the first project page

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Headpat Clicker",
+        "thumb": None,
+        "hero": None,
+        "categories": ["java", "game-dev"],
+        "slug": "headpat-clicker",
+        "prod": "https://itch.io",
+    },
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+]
+
+slug_project = {project["slug"]: project for project in projects}
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 
 @app.route("/about")
@@ -15,3 +36,12 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+    
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_projects:
+        abort(404)
+    return render_template(
+        f"project_{slug}.html",
+        project=slug_to_project[slug]
+    )
